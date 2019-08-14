465cb01c76141af72f6a005693fc59567d000e78
ishank <chauhanishank@gmail.com>
2019-08-14 13:52:23 +0530

    Sorting

---
+++ sorting/bubble.py
@@ -0,0 +1,9 @@
+'''def main():
+    r = int(input("Enter the number of elements"))
+    n = n[r]
+    for i in rang (r):
+        n[i] = int(input())
+    print (n)'''
+def faf():
+    a = input("I will print what you wrinte: ")
+    print(a)
